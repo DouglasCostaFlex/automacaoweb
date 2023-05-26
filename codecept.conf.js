1236c6f0a23a537a88f53aaa4e3654d9eb40a895
DouglasCostaMMII <DouglasCosta.flexmobile@gmail.com>
2023-05-26 11:58:21 -0300

    webupdtat

---
+++ codecept.conf.js
@@ -0,0 +1,24 @@
+const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+/** @type {CodeceptJS.MainConfig} */
+exports.config = {
+  tests: './steps/*_test.js',
+  output: './output',
+  helpers: {
+    WebDriver: {
+      url: 'https://192.168.1.128:9090/flextotal/login',
+      browser: 'chrome',
+      restart:false
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  name: 'ChromeTESTE'
+}
